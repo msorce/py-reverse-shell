56e0029367f145be624952395e91046fc9f47078
msorce <marksorce@gmail.com>
2016-06-05 22:44:29 -0700

    added client connection, receive encode decode commands

--- server.py
+++ server.py
@@ -45,4 +45,9 @@ def send_commands(conn):
             conn.send(str.encode(cmd))
             client_response = str(conn.recv(1024), "utf-8")
             print(client_response, end="")
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
 
+main()
